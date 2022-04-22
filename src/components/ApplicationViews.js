3d237dd7193d24e88447ffdb4e176938f750a1a2
karlarn <karlarnichols@gmail.com>
2022-04-22 10:54:57 -0500

    got the nutshell login to function

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,19 +1,17 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate, Outlet } from "react-router-dom"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 import { Home } from "./home/home"
 
-export const ApplicationView=({ isAuthenticated, setIsAuthenticated })=>{
-
-    const PrivateRoute = ({ children }) => {
-        return isAuthenticated ? children : <Navigate to="/login" />;
+export const ApplicationView = ({ isAuthenticated, setIsAuthenticated }) => {
+    const PrivateOutlet = () => {
+      return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
     }
-    
+  
     const setAuthUser = (user) => {
-        sessionStorage.setItem("encounter_user", JSON.stringify(user))
-        setIsAuthenticated(sessionStorage.getItem("encounter_user") !== null)
+      sessionStorage.setItem("enounter_user", JSON.stringify(user))
+      setIsAuthenticated(sessionStorage.getItem("encounter_user") !== null)
     }
-
 
     return (
         <>
@@ -21,7 +19,7 @@ export const ApplicationView=({ isAuthenticated, setIsAuthenticated })=>{
                 <Route exact path="/login" element={<Login setAuthUser={setAuthUser} />} />
                 <Route exact path="/register" element={<Register setAuthUser={setAuthUser} />} /> 
 
-                <Route exact path="/" element={<PrivateRoute><Home /></PrivateRoute>} /> 
+                <Route exact path="/" element={<PrivateOutlet><Home /></PrivateOutlet>} /> 
             </Routes>
         </>
         
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,80 +1,122 @@
-import React, { useRef } from "react"
-import { useNavigate } from "react-router-dom"
-import "./Login.css"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Register = ({setAuthUser}) => {
-    const firstName = useRef()
-    const lastName = useRef()
-    const email = useRef()
-    const conflictDialog = useRef()
-    const navigate = useNavigate()
+import "./Login.css";
 
+export const Register = () => {
+	const [registerUser, setRegisterUser] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+	});
+	const [conflictDialog, setConflictDialog] = useState(false);
 
-    const existingUserCheck = () => {
-        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
-            .then(res => res.json())
-            .then(user => !!user.length)
-    }
+	const navigate = useNavigate();
 
-    const handleRegister = (e) => {
-        e.preventDefault()
+	const handleInputChange = (event) => {
+		const newUser = { ...registerUser };
+		newUser[event.target.id] = event.target.value;
+		setRegisterUser(newUser);
+	};
 
+	const existingUserCheck = () => {
+		// If your json-server URL is different, please change it below!
+		return fetch(`http://localhost:8088/users?email=${registerUser.email}`)
+			.then((res) => res.json())
+			.then((user) => !!user.length);
+	};
 
-        existingUserCheck()
-            .then((userExists) => {
-                if (!userExists) {
-                    fetch("http://localhost:8088/users", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            email: email.current.value,
-                            name: `${firstName.current.value} ${lastName.current.value}`
-                        })
-                    })
-                        .then(res => res.json())
-                        .then(createdUser => {
-                            if (createdUser.hasOwnProperty("id")) {
-                                setAuthUser(createdUser)
-                                navigate("/")
-                            }
-                        })
-                }
-                else {
-                    conflictDialog.current.showModal()
-                }
-            })
-        
-    }
+	const handleRegister = (e) => {
+		e.preventDefault();
 
-    return (
-        <main style={{ textAlign: "center" }}>
+		existingUserCheck().then((userExists) => {
+			if (!userExists) {
+				// If your json-server URL is different, please change it below!
+				fetch("http://localhost:8088/users", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						email: registerUser.email,
+						name: `${registerUser.firstName} ${registerUser.lastName}`
+					}),
+				})
+					.then((res) => res.json())
+					.then((createdUser) => {
+						if (createdUser.hasOwnProperty("id")) {
+							// The user id is saved under the key nutshell_user in session Storage. Change below if needed!
+							sessionStorage.setItem("encounter_user", JSON.stringify(createdUser));
+							navigate("/");
+						}
+					});
+			} else {
+				setConflictDialog(true);
+			}
+		});
+	};
 
-            <dialog className="dialog dialog--password" ref={conflictDialog}>
-                <div>Account with that email address already exists</div>
-                <button className="button--close" onClick={e => conflictDialog.current.close()}>Close</button>
-            </dialog>
+	return (
+		<main style={{ textAlign: "center" }}>
+			<dialog className="dialog dialog--password" open={conflictDialog}>
+				<div>Account with that email address already exists</div>
+				<button
+					className="button--close"
+					onClick={(e) => setConflictDialog(false)}
+				>
+					Close
+				</button>
+			</dialog>
 
-            <form className="form--login" onSubmit={handleRegister}>
-                <h1 className="h3 mb-3 font-weight-normal">Please Register for Animal enCounter</h1>
-                <fieldset>
-                    <label htmlFor="firstName">First Name</label>
-                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="inputEmail">Email address</label>
-                    <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
-                </fieldset>
-                <fieldset>
-                    <button type="submit">Sign in</button>
-                </fieldset>
-            </form>
-        </main>
-    )
-}
+			<form className="form--login" onSubmit={handleRegister}>
+				<h1 className="h3 mb-3 font-weight-normal">
+					Please Register for Animal enCounter
+				</h1>
+				<fieldset>
+					<label htmlFor="firstName"> First Name </label>
+					<input
+						type="text"
+						name="firstName"
+						id="firstName"
+						className="form-control"
+						placeholder="First name"
+						required
+						autoFocus
+						value={registerUser.firstName}
+						onChange={handleInputChange}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="lastName"> Last Name </label>
+					<input
+						type="text"
+						name="lastName"
+						id="lastName"
+						className="form-control"
+						placeholder="Last name"
+						required
+						value={registerUser.lastName}
+						onChange={handleInputChange}
+					/>
+				</fieldset>
+				<fieldset>
+					<label htmlFor="inputEmail"> Email address </label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						className="form-control"
+						placeholder="Email address"
+						required
+						value={registerUser.email}
+						onChange={handleInputChange}
+					/>
+				</fieldset>
+				<fieldset>
+					<button type="submit"> Sign in </button>
+				</fieldset>
+			</form>
+		</main>
+	);
+};
 
