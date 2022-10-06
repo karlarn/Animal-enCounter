6e46d20992f6fe1dbc9ee628424d9b981bd3c8f6
karlarn <karlarnichols@gmail.com>
2022-10-06 09:51:31 -0500

    https://www.codewars.com/users/karlarn

--- src/manager/AnimalManager.js
+++ src/manager/AnimalManager.js
@@ -1,3 +1,9 @@
+// RETURNS ALL ANIMALS FROM DB RELATING TO A SINGLE USER ID AND SORTS IT BY DATE
+export const getAnimalByUserId = (currentUserId) => {
+    return fetch(`http://localhost:8088/animals?userId=${currentUserId}&_sort=date&_order=desc`)
+        .then(res => res.json())
+}
+
 // RETURNS ALL ANIMALS IN THE DB
 export const addAnimal = (newAnimal) => {
     return fetch('http://localhost:8088/animals', {
@@ -7,12 +13,6 @@ export const addAnimal = (newAnimal) => {
         },
         body: JSON.stringify(newAnimal)
     }).then(res => res.json())
-}
-
-// RETURNS ALL ANIMALS FROM DB RELATING TO A SINGLE USER ID AND SORTS IT BY DATE
-export const getAnimalByUserId = (currentUserId) => {
-    return fetch(`http://localhost:8088/animals?userId=${currentUserId}&_sort=date&_order=desc`)
-        .then(res => res.json())
 }
 
 // RETURNS ONE SPECIFIC ANIMAL FROM DB
